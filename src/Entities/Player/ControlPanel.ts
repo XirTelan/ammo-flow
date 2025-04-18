58cfbad1b014d0303c09870de10b0c942a4dd769
Xir <prssergey@gmail.com>
2025-04-18 23:41:41 +0300

    warehouse and turrets magagment logic into separate class . prob turrets will need they own

--- src/Entities/Player/ControlPanel.ts
+++ src/Entities/Player/ControlPanel.ts
@@ -1,20 +1,40 @@
 import { Scene } from "phaser";
 import { Warehouse } from "./Warehouse";
-import { TurretType } from "../../helpers/types";
+import { Turret } from "../Towers/Turret";
+import { Factory } from "./Factory";
+import { Artillery } from "../Towers/Artillery";
+import { MachineGun } from "../Towers/MachineGun";
+import { Game } from "../../scenes/Game";
+import { AllAmmoData } from "../../helpers/types";
 
 export class ControlPanel {
   scene: Scene;
-  warehouse: Warehouse;
+  warehouse: Warehouse = Warehouse.getInstance();
+  turrets: Turret[] = [];
+  factories: Factory[] = [];
 
   text: Phaser.GameObjects.Text;
-  constructor(scene: Scene) {
-    this.warehouse = Warehouse.getInstance();
-    this.text = scene.add.text(300, 700, "Ammo: 0", { fontSize: "16px" });
+  constructor(scene: Game) {
+    const { ammo }: { ammo: AllAmmoData } = scene.cache.json.get("ammo");
+    Warehouse.getInstance().initiateState(ammo);
+
+    this.text = scene.add.text(90, 650, "Ammo: 0", {
+      fontSize: "16px",
+    });
+
     this.warehouse.subscribe(`artillery.default`, (value) => {
       this.text.text = value;
     });
-  }
 
-  registerTurretType(turret: TurretType) {}
-  unregisterTurretType(turret: TurretType) {}
+    this.factories.push(new Factory(scene, "artillery", "default"));
+
+    this.turrets.push(new Artillery(scene, 1035, 850));
+    this.turrets.push(new Artillery(scene, 830, 1020));
+    this.turrets.push(new Artillery(scene, 1220, 1120));
+    this.turrets.push(new MachineGun(scene, 991, 1200));
+  }
+  update(time: number, delta: number) {
+    if (this.factories.length > 0)
+      this.factories.forEach((factory) => factory.update(time, delta));
+  }
 }
