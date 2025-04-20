6591679f8af3554ff3b371aab72a544a3a2a1768
Xir <prssergey@gmail.com>
2025-04-20 03:00:47 +0300

    change update to timeEvent

--- src/Entities/Projectile.ts
+++ src/Entities/Projectile.ts
@@ -1,7 +1,9 @@
 import { Scene } from "phaser";
+import { AmmoVariant } from "../helpers/types";
 
 export class Projectile extends Phaser.Physics.Arcade.Image {
   scene: Scene;
+  ammoData: AmmoVariant;
 
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, "projectile");
@@ -11,21 +13,32 @@ export class Projectile extends Phaser.Physics.Arcade.Image {
     scene.physics.add.existing(this);
     this.postFX.addBloom(0x0ffffff, 1, 1, 1.5, 4);
   }
-  initProj(x: number, y: number, speed: number, angle: number) {
+  initProj(x: number, y: number, angle: number, ammoData: AmmoVariant) {
     this.enableBody(true, x, y);
     this.setActive(true);
     this.setVisible(true);
+    this.ammoData = ammoData;
 
     const physBody = this.body as Phaser.Physics.Arcade.Body;
 
     physBody.setVelocity(0, 0);
     physBody.setAcceleration(0);
     physBody.onWorldBounds = true;
-    this.scene.physics.velocityFromRotation(angle, speed, physBody.velocity);
+    this.scene.physics.velocityFromRotation(
+      angle,
+      ammoData.speed,
+      physBody.velocity
+    );
     this.rotation = angle;
+    this.scene.time.addEvent({
+      delay: 100,
+      callback: this.updateRotation,
+      callbackScope: this,
+      loop: true,
+    });
   }
 
-  update() {
+  updateRotation() {
     const physBody = this.body as Phaser.Physics.Arcade.Body;
     const vx = physBody.velocity.x;
     const vy = physBody.velocity.y;
