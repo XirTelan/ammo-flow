9ac85ec8767575478cd352fc97999eddf92f2ef0
Xir <prssergey@gmail.com>
2025-04-25 20:21:22 +0300

    refactor: WorkersPanel

--- src/Entities/Player/ControlPanel/ui/WorkersPanel.ts
+++ src/Entities/Player/ControlPanel/ui/WorkersPanel.ts
@@ -8,6 +8,7 @@ const HEIGHT = 300;
 const NUMBERTEXT_STYLE = {
   fontSize: "48px",
   fontStyle: "bold",
+  fontFamily: "monospace",
 };
 
 export class WorkersPanel {
@@ -23,14 +24,14 @@ export class WorkersPanel {
     this.container = this.scene.add.container(x, y);
 
     this.totalWorkers = this.scene.add.text(
-      20,
+      30,
       100,
       controlPanel.workers.toString().padStart(2, "0"),
       NUMBERTEXT_STYLE
     );
     this.availableWorkers = this.scene.add.text(
-      20,
-      200,
+      30,
+      195,
       controlPanel.workersAvailable.toString().padStart(2, "0"),
       NUMBERTEXT_STYLE
     );
@@ -47,26 +48,7 @@ export class WorkersPanel {
     this.controlPanel.events.on("", this.updateTotalWorkers, this);
 
     this.container.add([
-      this.scene.add
-        .rectangle(0, 0, WIDTH, HEIGHT, colors.overlay.number)
-        .setOrigin(0),
-      this.scene.add
-        .rectangle(0, 60, 100, 40, colors.backgroundAccent.number)
-        .setOrigin(0),
-
-      this.scene.add
-        .rectangle(0, 150, 100, 40, colors.backgroundAccent.number)
-        .setOrigin(0),
-      this.scene.add.text(20, 70, "TOTAL", {
-        fontSize: "24px",
-        color: "#000",
-        fontStyle: "bold",
-      }),
-      this.scene.add.text(10, 100, "AVAILABE", {
-        fontSize: "18px",
-        color: "#000",
-        fontStyle: "bold",
-      }),
+      this.scene.add.rectangle(0, 50, WIDTH, HEIGHT, 0x444444).setOrigin(0),
       this.totalWorkers,
       this.availableWorkers,
     ]);
