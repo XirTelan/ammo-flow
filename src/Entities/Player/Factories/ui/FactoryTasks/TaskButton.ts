106f69e09a38ccaa788533e7f85357ce1c967474
Xir <prssergey@gmail.com>
2025-04-26 23:28:59 +0300

    TaskButton refactor

--- src/Entities/Player/Factories/ui/FactoryTasks/TaskButton.ts
+++ src/Entities/Player/Factories/ui/FactoryTasks/TaskButton.ts
@@ -3,7 +3,6 @@ import { colors } from "../../../../../helpers/config";
 
 export class TaskButton {
   private scene: Scene;
-  private showLines: boolean;
 
   container: Phaser.GameObjects.Container;
   btn: Phaser.GameObjects.GameObject;
@@ -19,40 +18,16 @@ export class TaskButton {
   ) {
     this.scene = scene;
 
-    this.showLines = showLines;
-
     const container = this.scene.add.container();
     const yPos = offset + 30;
 
-    if (this.showLines) {
+    if (showLines) {
       container.add(this.createLineGraphics(offset));
     }
 
-    const border = this.scene.add.rectangle(
-      100,
-      yPos,
-      width,
-      40,
-      colors.backgroundAccent.number
-    );
-
-    const bg = this.scene.add
-      .rectangle(
-        100,
-        yPos,
-        width - borderThickness * 2,
-        30,
-        colors.overlay.number
-      )
-      .setInteractive();
-
-    const text = this.scene.add
-      .text(100, yPos, title.toUpperCase(), {
-        color: "#fff",
-        fontStyle: "bold",
-        fontSize: "22px",
-      })
-      .setOrigin(0.5);
+    const border = this.createBorder(yPos, width);
+    const bg = this.createBackground(yPos, width, borderThickness);
+    const text = this.createText(yPos, title);
 
     this.setupHoverEffect(bg, text);
     bg.on("pointerup", action);
@@ -71,17 +46,64 @@ export class TaskButton {
     return graphics;
   }
 
+  private createBorder(
+    yPos: number,
+    width: number
+  ): Phaser.GameObjects.Rectangle {
+    return this.scene.add.rectangle(
+      100,
+      yPos,
+      width,
+      40,
+      colors.backgroundAccent.number
+    );
+  }
+
+  private createBackground(
+    yPos: number,
+    width: number,
+    borderThickness: number
+  ): Phaser.GameObjects.Rectangle {
+    return this.scene.add
+      .rectangle(
+        100,
+        yPos,
+        width - borderThickness * 2,
+        30,
+        colors.overlay.number
+      )
+      .setInteractive();
+  }
+
+  private createText(yPos: number, title: string): Phaser.GameObjects.Text {
+    return this.scene.add
+      .text(100, yPos, title.toUpperCase(), {
+        color: "#fff",
+        fontStyle: "bold",
+        fontSize: "22px",
+      })
+      .setOrigin(0.5)
+      .setShadow(0, 0, colors.accentHighlight.hex, 4, true, true);
+  }
+
   private setupHoverEffect(
     bg: Phaser.GameObjects.Rectangle,
     text: Phaser.GameObjects.Text
   ) {
     bg.on("pointerover", () => {
+      this.scene.sound.play("btnUiOver");
       bg.fillColor = colors.backgroundAccent.number;
       text.setColor("#444");
+      text.setShadow(0, 0, colors.accentHighlight.hex, 10, true, true);
     });
     bg.on("pointerout", () => {
       bg.fillColor = colors.overlay.number;
+
       text.setColor("#FFF");
+      text.setShadow(0, 0, colors.accentHighlight.hex, 4, true, true);
+    });
+    bg.on("pointerup", () => {
+      this.scene.sound.play("btnUiPress");
     });
   }
 }
